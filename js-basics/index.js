8d88b6918d16d20550c1bbc4cc10ff005c21b676
anant.gabhane@apexaiq.com <anant.gabhane@apexaiq.com>
2024-06-27 21:39:13 +0530

    js-learning

---
+++ js-basics/index.js
@@ -0,0 +1,16 @@
+// primitive type
+
+// const names = 4;
+// names = 1;
+// console.log(names);
+
+//ref type 
+
+// let name ='anant';
+// let age = '30';
+// person is a object 
+let person = {
+    name:'anant',
+    age:'30',
+}
+console.log(person);
